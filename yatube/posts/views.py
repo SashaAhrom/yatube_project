d27dc76d07f9e15ec2e94c70823023ee0856aa4c
Akhromenka <kartmander@gmail.com>
2021-10-12 11:49:19 +0300

    Add urls and view functions

---
+++ yatube/posts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Home page')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Group with name {slug}')
